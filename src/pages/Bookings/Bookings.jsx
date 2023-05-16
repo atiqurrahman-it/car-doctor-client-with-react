ac189ac5284f3d7012f70da82524980055e78868
atiqur <atiqurrahman.cse2552@gmail.com>
2023-05-17 03:01:14 +0600

    booking CRUD

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,53 +1,87 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProviders";
 import BookingsRow from "./BookingsRow";
+import Swal from "sweetalert2";
 
 const Bookings = () => {
-    const {user}=useContext(AuthContext)
-    const [bookings,setBookings]=useState([])
+  const { user } = useContext(AuthContext);
+  const [bookings, setBookings] = useState([]);
 
-    useEffect(()=>{
-        fetch(`http://localhost:5000/bookings?email=${user?.email}`)
-        .then(res=>res.json())
-        .then(data=>{
-            setBookings(data)
-            console.log(data)
-        })
-        .catch(error=>console.log(error))
-    },[])
-    return (
-        <div>
-            <h1 className="text-3xl font-bold">your Bookings : {bookings.length}</h1>
-            <div className="overflow-x-auto w-full">
-  <table className="table w-full">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <th>Image</th>
-        <th>Name</th>
-        <th>Price</th>
-        <th> Date</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-        {
-            bookings.map(booking=><BookingsRow booking={booking} key={booking._id}></BookingsRow>)
+  const handelBookingStatus = (id) => {
+    fetch(`http://localhost:5000/bookings/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ status: "confirm" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          const remaining = bookings.filter((booking) => booking._id !== id);
+          const update = bookings.find((booking) => booking._id === id);
+          update.status='confirm'
+          const newBooking = [update, ...remaining];
+          setBookings(newBooking);
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Booking Status update ",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+
         }
-     
-    </tbody>
-    {/* foot */}
-  
-    
-  </table>
-</div>
-        </div>
-    );
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/bookings?email=${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setBookings(data);
+        // console.log(data)
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">your Bookings : {bookings.length}</h1>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Image</th>
+              <th>Service</th>
+              <th>Price</th>
+              <th> Date</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.map((booking) => (
+              <BookingsRow
+                handelBookingStatus={handelBookingStatus}
+                bookings={bookings}
+                setBookings={setBookings}
+                booking={booking}
+                key={booking._id}
+              ></BookingsRow>
+            ))}
+          </tbody>
+          {/* foot */}
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Bookings;
--- src/pages/Bookings/BookingsRow.jsx
+++ src/pages/Bookings/BookingsRow.jsx
@@ -1,29 +1,80 @@
-const BookingsRow = ({booking}) => {
-    console.log(booking)
-    const {img,date,price,service}=booking
+import Swal from "sweetalert2";
+
+const BookingsRow = ({ booking,bookings,setBookings,handelBookingStatus }) => {
+  const { _id, img, date, price, service,status } = booking;
+
+  const handelDeleteBooking = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        fetch(`http://localhost:5000/bookings/${id}`,{
+            method:"DELETE"
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            if(data.deletedCount >0){
+                const remainingBooking=bookings.filter(book=>book._id!==id)
+                setBookings(remainingBooking)
+
+                Swal.fire("Deleted!", "Your file has been deleted.", "success");
+            }
+        })
+        .catch(error=>console.log(error))
+
+
+      }
+    });
+  };
   return (
     <>
       <tr>
         <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
+          <button
+            onClick={() => handelDeleteBooking(_id)}
+            className="btn btn-sm btn-circle"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
         </th>
 
         <td>
           <div className="avatar">
             <div className="rounded w-24 h-24">
-              <img src={img} alt="Avatar Tailwind CSS Component"/>
+              <img src={img} alt="Avatar Tailwind CSS Component" />
             </div>
           </div>
         </td>
 
-        <td>   {service &&  <> {service} </>} </td>
+        <td> {service && <> {service} </>} </td>
         <td> $ {price} </td>
         <td>{date}</td>
 
         <th>
-          <button className="btn btn-warning">pending</button>
+            {
+                status=='confirm' ?   <button className="btn btn-info">confirm </button> :
+               <button onClick={()=>handelBookingStatus(_id)} className="btn btn-warning">pending</button>
+            }
         </th>
       </tr>
     </>
